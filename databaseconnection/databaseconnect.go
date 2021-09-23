c1d6cb3f0121882f106c9e4c5556631b49d2effc
Nicolai Mördrup <nicolaim@MacBook-Pro-som-tillhor-Nicolai.local>
2021-09-23 13:50:51 +0200

    fixed some structure regarding the database

--- databaseconnection/databaseconnect.go
+++ databaseconnection/databaseconnect.go
@@ -10,6 +10,9 @@ import (
 	_ "modernc.org/sqlite"
 )
 
+// ConnectToDatabase connects to the database an checks if the file is already
+// created or not.
+//TODO fix such that the database handles errors correct.
 func ConnectToDatabase() {
 	db, _ := sql.Open("sqlite", "./librarystorage.db")
 
@@ -21,6 +24,19 @@ func ConnectToDatabase() {
 	var updatedTime string = time.Now().String()
 	stmt.Exec("book_2", createdTime, updatedTime)
 	rows, _ := db.Query("SELECT isbn, title ,createTime, updateTime FROM library")
+
+	ReadDatabase(rows)
+
+}
+
+// DatabaseQuery Prepers a database query and executes the query on the
+// database. It takes as input a query string and gives as output the rows
+func DatabaseQuery() /* *sql.Rows */ {
+	//TODO create the code for this
+}
+
+// ReadDatabase reads the information that we get from the database.
+func ReadDatabase(rows *sql.Rows) {
 	var isbn int
 	var title string
 	var createTime string
@@ -30,5 +46,14 @@ func ConnectToDatabase() {
 		rows.Scan(&isbn, &title, &createTime, &updateTime)
 		fmt.Println(strconv.Itoa(isbn) + ": " + title + ": " + createTime + ": " + updateTime)
 	}
+}
 
+// DeleteDatabase deletes the database. Takes a database as input
+func DeleteDatabase(db *sql.DB) {
+	//TODO create the code for this
+}
+
+// CreateDatabase creates the table if it is not already created
+func CreateDatabase() {
+	//TODO create the code for this
 }
